c3537de2fa8973ef1afddaedbf0a7d12718a6c9d
Rustam Dzhamilov <rdzhamilovv@gmail.com>
2024-08-18 19:31:55 +0200

    Error related to Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
-import { Categories, Container, SortPopup, Title } from "@/components/shared";
+import { Categories, Container, Filters, Title } from "@/components/shared";
+import { ProductCard } from "@/components/shared/product-card";
+import { TopBar } from "@/components/shared/top-bar";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title className="font-extrabold" text="Все пиццы" size="lg" />
+      </Container>
+      <TopBar />
 
-        <Categories />
-        <SortPopup />
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              <ProductCard
+                id={0}
+                name="Чизбургер-пицца"
+                price={555}
+                imageUrl={
+                  "https://media.dodostatic.net/image/r:584x584/11EE7D611ADF5AAD898B8B651186E023.avif"
+                }
+              />
+            </div>
+          </div>
+        </div>
       </Container>
     </>
   );
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,5 +1,7 @@
+export { TopBar } from './top-bar';
 export { Title } from './title';
 export { Container } from './container';
 export { Header } from './header';
 export { Categories } from './categories'
-export { SortPopup } from './sort-popup'
+export { Filters } from './filters'
+export { FilterCheckbox } from './filter-checkbox'
