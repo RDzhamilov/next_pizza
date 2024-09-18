b597d241009e4d7833af9853c20f8aa36942abc8
Rustam Dzhamilov <rdzhamilovv@gmail.com>
2024-09-18 18:29:04 +0200

    search by fitr and improvement of the function of adding goods to cart are implemented

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,10 +1,24 @@
-import { Container, PizzaImage, Title } from "@/shared/components/shared";
-import { GroupVariants } from "@/shared/components/shared/group-variants";
+import { Container, ProductForm } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
-  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+  const product = await prisma.product.findFirst({
+    where: { id: Number(id) },
+    include: {
+      ingredients: true,
+      category: {
+        include: {
+          products: {
+            include: {
+              items: true,
+            },
+          },
+        },
+      },
+      items: true,
+    },
+  });
 
   if (!product) {
     return notFound();
@@ -12,34 +26,7 @@ export default async function ProductPage({ params: { id } }: { params: { id: st
 
   return (
     <Container className="flex flex-col my-10">
-      <div className="flex flex-1">
-        <PizzaImage imageUrl={product.imageUrl} size={40} />
-
-        <div className="w-[490px] bg-[#f7f6f5] p-7">
-          <Title text={product.name} size="md" className="font-extrabold mb-1" />
-
-          <p className="text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-
-          <GroupVariants
-            value="2"
-            items={[
-              {
-                name: "Маленькая",
-                value: "1",
-              },
-              {
-                name: "Средняя",
-                value: "2",
-              },
-              {
-                name: "Большая",
-                value: "3",
-                disabled: true,
-              },
-            ]}
-          />
-        </div>
-      </div>
+      <ProductForm product={product} />
     </Container>
   );
 }
