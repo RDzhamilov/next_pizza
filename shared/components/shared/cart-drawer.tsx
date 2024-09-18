b37b61acce86c26e5e8aacd18ceb9a22555ec0ea
Rustam Dzhamilov <rdzhamilovv@gmail.com>
2024-09-18 21:19:53 +0200

    bug cart

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -47,61 +47,70 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children,
   return (
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
+
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
-        <SheetHeader>
-          <SheetTitle>
-            В корзине <span className="font-bold">{items.length} товара</span>
-          </SheetTitle>
+        {totalAmount > 0 && (
+          <SheetHeader>
+            <SheetTitle>
+              В корзине <span className="font-bold">{items.length} товара</span>
+            </SheetTitle>
 
-          {/* just to avoid making a mistake */}
-          <VisuallyHidden>
-            <SheetDescription />
-          </VisuallyHidden>
-        </SheetHeader>
+            {/* aria-describedby={undefined} */}
+            {/* just to avoid making a mistake */}
+            <VisuallyHidden>
+              <SheetDescription />
+            </VisuallyHidden>
+          </SheetHeader>
+        )}
 
-        <div className="-mx-6 mt-5 overflow-auto flex-1">
-          {items.map((item) => (
-            <div key={item.id} className="mb-2">
-              <CartDrawerItem
-                id={item.id}
-                imageUrl={item.imageUrl}
-                details={
-                  item.pizzaSize && item.pizzaType
-                    ? getCartItemDetails(
-                        item.ingredients,
-                        item.pizzaType as PizzaType,
-                        item.pizzaSize as PizzaSize
-                      )
-                    : ""
-                }
-                name={item.name}
-                price={item.price}
-                quantity={item.quantity}
-                onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
-                onClickRemove={() => removeCartItem(item.id)}
-              />
-            </div>
-          ))}
-        </div>
-
-        <SheetFooter className="-mx-6 bg-white p-8">
-          <div className="w-full">
-            <div className="flex mb-4">
-              <span className="flex flex-1 text-lg text-neutral-500">
-                Итого
-                <div className="flex-1 border-b  border-dashed border-b-neutral-200 relative -top-1 mx-2" />
-              </span>
-              <span className="font-bold text-lg">{totalAmount} $</span>
+        {totalAmount > 0 && (
+          <>
+            <div className="-mx-6 mt-5 overflow-auto flex-1">
+              {items.map((item) => (
+                <div key={item.id} className="mb-2">
+                  <CartDrawerItem
+                    id={item.id}
+                    imageUrl={item.imageUrl}
+                    details={
+                      item.pizzaSize && item.pizzaType
+                        ? getCartItemDetails(
+                            item.ingredients,
+                            item.pizzaType as PizzaType,
+                            item.pizzaSize as PizzaSize
+                          )
+                        : ""
+                    }
+                    disabled={item.disabled}
+                    name={item.name}
+                    price={item.price}
+                    quantity={item.quantity}
+                    onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
+                    onClickRemove={() => removeCartItem(item.id)}
+                  />
+                </div>
+              ))}
             </div>
 
-            <Link href="/cart">
-              <Button type="submit" className="w-full h-12 text-base">
-                Оформить заказ
-                <ArrowRight className="w-5 ml-2" />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
+            <SheetFooter className="-mx-6 bg-white p-8">
+              <div className="w-full">
+                <div className="flex mb-4">
+                  <span className="flex flex-1 text-lg text-neutral-500">
+                    Итого
+                    <div className="flex-1 border-b  border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+                  </span>
+                  <span className="font-bold text-lg">{totalAmount} $</span>
+                </div>
+
+                <Link href="/cart">
+                  <Button type="submit" className="w-full h-12 text-base">
+                    Оформить заказ
+                    <ArrowRight className="w-5 ml-2" />
+                  </Button>
+                </Link>
+              </div>
+            </SheetFooter>
+          </>
+        )}
       </SheetContent>
     </Sheet>
   );
