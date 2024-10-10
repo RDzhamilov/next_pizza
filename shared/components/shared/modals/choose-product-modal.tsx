e44f12cc4257a5b50792bc2c89f4f486d4c3f137
Rustam Dzhamilov <rdzhamilovv@gmail.com>
2024-10-10 17:32:52 +0200

    adding a new page checkout

--- shared/components/shared/modals/choose-product-modal.tsx
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -5,7 +5,6 @@ import { cn } from "@/shared/lib/utils";
 import React from "react";
 import { useRouter } from "next/navigation";
 import { ProductWithRelations } from "@/@types/prisma";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden"; // Импорт компонента для скрытия
 import { ProductForm } from "../product-form";
 
 interface Props {
@@ -23,13 +22,7 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
           "p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden",
           className
         )}
-        aria-describedby={undefined} //just to avoid making a mistake
       >
-        {/* just to avoid making a mistake */}
-        <VisuallyHidden>
-          <DialogTitle>Product Details</DialogTitle>
-        </VisuallyHidden>
-
         <ProductForm product={product} onSubmit={() => router.back()} />
       </DialogContent>
     </Dialog>
