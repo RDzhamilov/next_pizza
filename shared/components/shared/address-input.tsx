64023706c579d6d0d0351f12b295c9abfe89769a
Rustam Dzhamilov <rdzhamilovv@gmail.com>
2024-11-11 13:27:57 +0100

    add GooglePlacesAutocomplete

--- shared/components/shared/address-input.tsx
+++ shared/components/shared/address-input.tsx
@@ -1,22 +1,52 @@
-"use client";
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
-import React from "react";
-import { AddressSuggestions } from "react-dadata";
-import "react-dadata/dist/react-dadata.css";
-
-{
-  /* TODO где-то тут ошибки */
+interface Option {
+  label: string;
+  value: string;
 }
 
 interface Props {
-  onChange?: (value?: string) => void;
+  onChange?: (address: string) => void;
 }
 
 export const AddressInput: React.FC<Props> = ({ onChange }) => {
+  const [value, setValue] = useState<Option | null>(null);
+
   return (
-    <AddressSuggestions
-      token="b5b8bb983ddcd08648080e0271d9dd367bb7aa65"
-      onChange={(data) => onChange?.(data?.value)}
+    <GooglePlacesAutocomplete
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
+      selectProps={{
+        value,
+        onChange: (newValue: Option | null) => {
+          setValue(newValue);
+          if (onChange && newValue) {
+            onChange(newValue.label);
+          }
+        },
+
+        placeholder: "Enter the address",
+
+        styles: {
+          control: (baseStyles, state) => ({
+            ...baseStyles,
+            borderColor: state.isFocused ? "blue" : "grey",
+            borderRadius: "10px",
+            boxShadow: state.isFocused ? "0 0 5px rgba(0, 0, 255, 0.5)" : "none",
+          }),
+          menu: (base) => ({
+            ...base,
+            borderRadius: "10px",
+            boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+          }),
+          option: (base, state) => ({
+            ...base,
+            backgroundColor: state.isSelected ? "lightblue" : "transparent",
+            color: state.isSelected ? "darkblue" : "black",
+            padding: "10px",
+          }),
+        },
+      }}
     />
   );
 };
